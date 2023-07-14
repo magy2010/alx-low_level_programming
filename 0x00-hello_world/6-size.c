cef1433ad1ecc5da18cc9152f62d234dbc2de060
magy2010 <magdafathiattia@gmail.com>
2023-07-13 20:54:37 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+**/
+*main - description
+*
+*Return: Always 0
+*/
+int main(void)
+{
+printf("int size:%1d\n",sizeof(int));
+printf("char size:%1d\n",sizeof(char));
+printf("long int size:%1d\n",sizeof(long int));
+printf("long long int:%1d\n",sizeof(long long int));
+printf("float size:%1d\n",sizeof(float));
+return (0);
+}
+
