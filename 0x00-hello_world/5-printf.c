12e8d0f4aac4f5dce2d9faaa38c22700b5e2f495
magy2010 <magdafathiattia@gmail.com>
2023-07-13 20:15:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
 *main - Entry point
 *
-* Return: 0
+* Return: Always 0
 */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
