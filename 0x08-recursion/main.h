f2bfd48fa696be96b257b4996577f445cca492a2
magy2010 <magdafathiattia@gmail.com>
2023-08-03 08:14:42 -0700

    s

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -11,4 +11,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-#endif
+#endif /* MAIN_H */
