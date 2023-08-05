38ccba6f8dc6b7b0e2c4c7cc6a5c0160e4838c97
magy2010 <magdafathiattia@gmail.com>
2023-08-05 15:12:52 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  *
  * Return: Always 0 (success)
  */
-int main(int argc ـــattributeـــ((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s\n", *array);
-
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
