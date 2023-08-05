3e77b25652d789203adfe2bc12800f0890430c13
magy2010 <magdafathiattia@gmail.com>
2023-08-05 02:54:11 -0700

    libmy

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,21 @@
 #include"main.h"
 
 /**
- * main: prints_alphabet_x10 - function that prints the alphabet 10 times
- * Description: print_alphabet_x10
+ * prints_alphabet_x10 - function that prints the alphabet 10 times
+ * 
  *
 */
 
 void print_alphabet_x10(void)
 
 {
-
 	int i;
 
 	for (i = 1; i <= 10; i++)
-
 	{
-
 		char j = 'a';
-		while (j <= 'z')
 
+		while (j <= 'z')
 		{
 			_putchar(j);
 			j++;
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * @val: square root
+ * Return: int
+ */
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+`
