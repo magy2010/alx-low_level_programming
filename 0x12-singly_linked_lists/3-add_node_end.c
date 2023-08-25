d0bf024108e2030f6518e76b4a463a325ade21c8
Magda Fathi <magdafathiattia@gmail.com>
2023-08-25 08:04:19 -0700

    sda

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,23 +1,43 @@
 #include "lists.h"
 /**
- * add_node_end - adds a new node at the end of a  list.
+ * add_node_end - adds a new node at the end of a list.
  * @head: address of pointer to head node
- * @str: field of node
- * Return: size of list
+ * @str: string to be added to the list
+ * Return: NULL in case of failure
+ * or address or element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *node = *head;
+	char *dup;
+	int len;
+	list_t *new, *last;
 
-	if (!head || !new_node)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	if (str)
+
+	dup = strdup(str);
+	if (str == NULL)
 	{
-		new_node->str = strdup(str);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		jkjkjkjkjkjkfjfhgjukjgjkghgljk.;j.hlkj
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
+}
