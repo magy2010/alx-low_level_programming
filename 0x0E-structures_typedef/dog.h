a376cabeba9e7a98105511e08543a2efe95452b7
Magda Fathi <magdafathiattia@gmail.com>
2023-08-26 15:05:55 -0700

    dsf

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * struct dog - a dog struct
+ * struct dog - a new type of data
+ * representing a dog
  * @name: dog's name
  * @age: dog's age
  * @owner: owner's name
@@ -15,11 +19,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
