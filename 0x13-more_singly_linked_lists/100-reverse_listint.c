c1056a81dd2137a185d980bbdcda4f3765d25168
Magda Fathi <magdafathiattia@gmail.com>
2023-08-29 16:49:11 -0700

    l

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,16 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *node = NULL, *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	if (!head || !*head)
-		return (NULL);
-	node = *head;
-	*head = NULL;
-	while (node)
+	while (*head)
 	{
-		next = node->next;
-		node->next = *head;
-		*head = node;
-		node = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
+	*head = prev;
 	return (*head);
 }
